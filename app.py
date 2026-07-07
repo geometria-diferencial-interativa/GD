70b9e4935a1bd6fb88ab8886c56cbe478e4966d0
geometria-diferencial-interativa <geometriadiferencialinterativa@gmail.com>
2026-07-07 13:25:51 -0300

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 # CONFIGURAÇÃO GERAL DA PÁGINA
 # ============================================================
 
+
 st.set_page_config(
     page_title="Geometria Diferencial Interativa",
     page_icon="📐",
