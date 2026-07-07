b9b0f0a9bf89b11facfa8db73a9ef1ec0d01593f
geometria-diferencial-interativa <geometriadiferencialinterativa@gmail.com>
2026-07-07 19:48:11 -0300

    03_Plano_Tangente_Normal.py

--- pages/03_Plano_Tangente_Normal.py
+++ pages/03_Plano_Tangente_Normal.py
@@ -326,7 +326,7 @@ def make_plot(
     return fig
 
 
-st.title("Plano Tangente e Vetor Normal")
+st.title("3 - Plano Tangente e Vetor Normal")
 
 st.write("Neste módulo estudamos a geometria local de uma superfície regular em torno de um ponto.")
 
