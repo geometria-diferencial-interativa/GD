e3d9b97179060b3a16a1a2bafcecb8a8f8518c75
geometria-diferencial-interativa <geometriadiferencialinterativa@gmail.com>
2026-07-07 23:03:35 -0300

    01_Curvas.py

--- pages/01_Curvas.py
+++ pages/01_Curvas.py
@@ -14,7 +14,7 @@ with st.sidebar:
     st.page_link("pages/01_Curvas.py", label="1. Curvas em R³")
     st.page_link("pages/02_Superficies_Regulares.py", label="2. Superfícies Regulares")
     st.page_link("pages/03_Plano_Tangente_Normal.py", label="3. Plano Tangente e Vetor Normal")    
-    st.page_link("pages/04_Primeira_Forma_Fundamental.py", label="4. Primeira Forma Fundamental")    
+    st.page_link("pages/04_Formas_Fundamentais.py", label="4. Formas Fundamentais")
     st.markdown("5. Segunda Forma Fundamental")
     st.markdown("6. Curvaturas")
     st.markdown("7. Superfícies Mínimas")
